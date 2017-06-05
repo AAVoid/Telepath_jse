7988dc1985da2de4690fa23a9cb09ff3db0f9eeb
AAVoid <tepoo17@hotmail.com>
2017-06-04 21:35:43 -0400

    Intégration méthodes de ciffrement depuis projet Alpha

--- src/fr/telepath/modele/AlphaUneCle.java
+++ src/fr/telepath/modele/AlphaUneCle.java
@@ -1,40 +1,83 @@
 package fr.telepath.modele;
 
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 //AUTEUR : Aymerik ABOSO , MASTER 1 MIAGE , Université des Antilles - Guadeloupe
 //2016 - 2017
 
 
 
-//POUR POUVOIR CHIFFRER LES MESSAGES RECU ET ENVOYES
+//Classe implémentant le chiffrement Alpha que j'ai créé
+//Alpha est un algorithme de chiffrement symétrique par bloc
 public class AlphaUneCle {
-	//private static final int MODULO_XOR_INT = 65_536;
-	private static final int MODULO_XOR_BYTE = 128;
-	private static final int SALT_XOR = 1;
+	private static final int SALT_XOR = 13 + 14 + 17;
 
-	//XOR ENTRE DEUX BYTE
+	//XOR entre deux bytes
 	private static byte XOR_Byte_Byte(byte A, byte B) {
 		return (byte)(A ^ B);
 	}
 
-	//CHIFFREMENT D'UN BYTE PAR UNE CLE BYTE[] AVEC UN SALAGE
+	//Chiffrement d'un byte par une clé byte[]
 	private static byte XOR_Byte(byte caractere, byte[] cle, int saltNumCaracBloc) {
 		byte charChiff = caractere;
-		int taille = cle.length; //TAILLE < 46 339 POUR EVITER DEBORDEMENT INT AVEC i * i
+		int taille = cle.length;
 		for(int i = 0 ; i < taille ; i++) {
 			charChiff = XOR_Byte_Byte(charChiff, cle[i]);
-			charChiff = XOR_Byte_Byte(charChiff, (byte)(((i * i ) + SALT_XOR) % MODULO_XOR_BYTE)); //Salage
+			charChiff = XOR_Byte_Byte(charChiff, (byte)((i * i) + SALT_XOR)); 
+			//Salage avec i et SALT_XOR pour éviter que la répétition d'un caractère 
+			//de la clé annule le XOR avec ce caractère identique de la clé utilisé précédemment pour le XOR
 		}
-		return XOR_Byte_Byte(charChiff, (byte)saltNumCaracBloc); //Salage
+		return XOR_Byte_Byte(charChiff, (byte)saltNumCaracBloc); 
+		//Salage avec la position du caractère à chiffrer dans le bloc à chiffrer, pour éviter que 
+		//la répétition d'un caractère de la chaine à chiffrer donne le même résultat
 	}
 
-	//CHIFFREMENT D'UNE CHAINE BYTE[] PAR UNE CLE BYTE[]
-	public static byte[] XOR_Chiff_ByteS(byte[] chaine, byte[] cle) { 
-		byte[] chiffre = new byte[chaine.length]; //TAILLE < 46 339 POUR EVITER DEBORDEMENT INT AVEC i * i
+	//Retourne le SHA256 de chaine
+	public static String hasher_SHA_256(String chaine) {
+		String hash = "";
+		MessageDigest messageDigest = null;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		messageDigest.update(chaine.getBytes());
+		byte[] digest = messageDigest.digest();
+		hash = new String(digest);
+		return hash;
+	}
+
+	//Retourne le SHA1 de chaine
+	public static String hasher_SHA_1(String chaine) {
+		String hash = "";
+		MessageDigest messageDigest = null;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-1");
+		} 
+		catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		messageDigest.update(chaine.getBytes());
+		byte[] digest = messageDigest.digest();
+		hash = new String(digest);
+		return hash;
+	}
+
+	//Chiffrement d'un byte[] par une clé byte[] : Publique (SHA256)
+	//Le chiffrement via cette méthode et celui pour les fichiers n'est pas le même
+	//Le chiffrement pour le fichier est différent étant donné le chiffrement de grands volumes
+	//de données
+	public static byte[] chiffrer(byte[] chaine, byte[] cle) { 
+		byte[] chiffre = new byte[chaine.length];
 		int taille = chaine.length;
+		//Chiffrement par la clé
 		for(int i = 0 ; i < taille ; i++)
-			chiffre[i] += XOR_Byte(chaine[i], cle, i);
+			chiffre[i] = XOR_Byte(chaine[i], cle, i);
+		//Chiffrement par un SHA256 de la clé pour être sûr d'éviter une collision
+		cle = hasher_SHA_256(new String(cle)).getBytes();
+		for(int i = 0 ; i < taille ; i++)
+			chiffre[i] = XOR_Byte(chiffre[i], cle, i);
 		return chiffre;
 	}
 }
